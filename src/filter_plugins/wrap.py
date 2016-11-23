92b1a13a8f7844411de6711b7c89c4ef5c7ad449
Aaron Caldiero <Aaron@1strategy.com>
2016-11-23 14:50:38 -0700

    Delete wrap.py

--- src/filter_plugins/wrap.py
+++
@@ -1,9 +0,0 @@
-# when using, use convert_data:False in lookup per https://github.com/ansible/ansible/issues/11885
-def wrap(list):
-    return [ '"' + x + '"' for x in list]
-
-class FilterModule(object):
-    def filters(self):
-        return {
-            'wrap': wrap
-        }
